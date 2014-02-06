50568f8b49ddb448bcd4d89a080f27e92d98a334
Splintex <godun.yevgen@gmail.com>
2014-02-06 14:58:50 +0200

    upd

--- js/common.js
+++ js/common.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-
+	var subnav = $(".js-subnav");
+	//$(".js-nav").find("li:first").next().addClass("js-show-subnav");
+	$(".js-show-subnav").hover(function(){
+		subnav.toggle(); 
+	});
+	subnav.hover(function(){
+		subnav.toggle(); 
+	});
 });
